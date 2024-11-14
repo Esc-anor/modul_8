1e7cbd23b51fc4f86384626245a1c5224d73b88d
Роман <romangribanov@rambler.ru>
2024-11-15 01:24:16 +0300

    Упрощение кода

--- module_8_1.py
+++ module_8_1.py
@@ -1,20 +1,10 @@
 def add_everything_up(a, b):  # будет складывать числа(int, float) и строки(str)
-    # блок try проверки работы кода на ошибки и если ошибки нет, вернет result
     try:
-        # условие проверки, что a и b - числа
-        if (isinstance(a, (int, float))) is True and (isinstance(b, (int, float))) is True:
-            # присвоение переменной result суммы чисел с округлением до 3 знаков после запятой
-            result = round(a + b, 3)
-        else:
-            result = a + b
-        # возврат работы функции когда ошибки нет
-        return result
-    # блок except вернет строковое значение в случае разных типов переменных на входе
+        # возврат работы функции когда ошибки нет, суммы чисел с округлением до 3 знаков после запятой
+        return round(a + b, 3)
     except TypeError:
-        # присвоение переменной result строковых значений суммы a, b
-        result = (str(a) + str(b))
         # возврат работы функции при разных типах входных данных
-        return result
+        return str(a) + str(b)
 
 
 print(add_everything_up(123.456, 'строка'))
